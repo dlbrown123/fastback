3840fad7dbfb57729cb4447efe422e36ed567502
Erik Wiffin <erik.wiffin@gmail.com>
2013-03-03 11:07:27 -0500

    Setup dynamic feed

--- static/js/main.js
+++ static/js/main.js
@@ -33,4 +33,30 @@ $(function () {
 		$('.comment-simple').slideUp();
 		$('.comment-complex').slideUp();
 	});
+
+	var triggerUpdate, updateFeed;
+
+	triggerUpdate = function (index, element) {
+		var tID = window.setTimeout(function (element) {
+			$.getJSON('/feed', $.proxy(updateFeed, $(element)))
+				.complete(function () {triggerUpdate(0, element);});
+		}, 2000, element);
+	}
+
+	updateFeed = function (data) {
+		var el, i,
+			template = _.template($('#feed-item-template').html());
+
+		for (i in data) {
+			el = this.find('.feed-item[rel="' + data[i]['id'] + '"]');
+			if (el.length > 0) continue;
+			el = template({obj: data[i]});
+			this.find('h2').after(el);
+		}
+
+		this.find('.feed-item').slideDown();
+		this.find('.feed-item:nth-child(n+10)').remove();
+	};
+
+	$('.the-feed').each(triggerUpdate);
 });
